e1cbff1bb770513950dbf6001bbfa096ed59f0da
Areeba <areebayasmeen11@gmail.com>
2024-03-26 16:52:10 +0500

    Queue implementation

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@
 public class Main {
     public static void main(String[] args) {
         Queue2<Integer> queue = new Queue2<>();
-       queue.enqueue(12);
-        queue.enqueue(13);
+    queue.enqueue(12);
+       queue.enqueue(13);
         queue.enqueue(14);
         queue.enqueue(15);
         queue.enqueue(16);
         System.out.println("de_queueing");
         System.out.println(queue.dequeue());
         System.out.println(queue.dequeue());
+        System.out.println("displaying front");
+        queue.front();
 
 
     }}
